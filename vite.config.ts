c8345321412725e371f964ab6151627e9b9ce813
ktydg <annatars@ktydg>
2024-03-17 16:21:29 +0300

    Forgot to change base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr({include: "**/*.svg?react",}), react()],
-  base: "/ViReT/",
+  base: "/vkForm/",
   resolve: {
     alias: {
       '~app': path.resolve('src/app'),
